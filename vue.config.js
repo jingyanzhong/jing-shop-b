79027ef7d62cdc84653d0660595eb4c4aee4faf6
jingyan0721 <melody804210@gmail.com>
2023-02-07 18:25:28 +0800

    20230207 修改title圖示-favicon.ico

--- vue.config.js
+++ vue.config.js
@@ -10,8 +10,8 @@ module.exports = {
           '京式會社為代購日本和韓國品牌服飾及雜貨，因全部都為日本、韓國空運來台，需等待至少7-20個工作天才能出貨，煩請下單的小仙女們耐心等候，美好的事物值得等待。',
         og: {
           type: 'website',
-          url: 'https://a94s84.github.io/Hermosa/#/',
-          title: 'Hermosa',
+          // url: 'https://a94s84.github.io/Hermosa/#/',
+          title: '京式會社',
           image: '@/public/favicon.ico',
           description:
             '京式會社為代購日本和韓國品牌服飾及雜貨。'
